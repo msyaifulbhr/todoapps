3e637f732e7440351c7a842f829bbd81259add52
msyaifulbhr <syaifulyy@gmail.com>
2023-09-09 21:07:46 +0700

    getData from localStorage

--- js/script.js
+++ js/script.js
@@ -15,12 +15,29 @@ document.addEventListener(SAVED_EVENT, function () {
     console.log(localStorage.getItem(STORAGE_KEY));
 });
 
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if (data !== null) {
+        for (const todo of data) {
+            todos.push(todo);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const submitForm = document.getElementById('form');
     submitForm.addEventListener('submit', function (event) {
         event.preventDefault();
         addTodo();
     });
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    }
 });
 
 function saveData() {
@@ -169,3 +186,4 @@ function findTodoIndex(todoId) {
     }
     return -1;
 }
+
